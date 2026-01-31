1be1363f91f74575d57f59b7cbb5ec7b8cf4cc6e
louiseeriksen <157900426+louiseeriksen@users.noreply.github.com>
2026-01-31 10:05:48 +0100

    Added API correspondency with the current HTML of Humidity and Temperature

--- src/app.js
+++ src/app.js
@@ -1,22 +1,26 @@
-function ChangeInfo(response) {
+function changeDisplay(response) {
   console.log(response.data);
 
   let h1 = document.querySelector("h1");
   let temperature = document.querySelector("#degrees");
+  let humidity = document.querySelector("#humidity");
+  let wind = document.querySelector("#wind");
 
   h1.innerHTML = response.data.city;
-  temperature.innerHTML = response.data.temperature.current;
+  temperature.innerHTML = Math.round(response.data.temperature.current);
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  wind.innerHTML = `${Math.round(response.data.wind.speed * 10) / 10}km/h`;
 }
 
 function userSubmit(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
   console.log(cityInput.value);
-  axios.get(apiUrl).then(ChangeInfo);
-}
 
-let apiKey = `033479d334coa33b0517ta6857d46eaf`;
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityInput.value}&key=${apiKey}`;
+  let apiKey = `033479d334coa33b0517ta6857d46eaf`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityInput.value}&key=${apiKey}`;
+  axios.get(apiUrl).then(changeDisplay);
+}
 
 let cityForm = document.querySelector("#city-form");
 cityForm.addEventListener("submit", userSubmit);
