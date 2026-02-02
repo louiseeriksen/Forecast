a6957fc69505ad1b17b549c1cac6df4d70089ab3
louiseeriksen <157900426+louiseeriksen@users.noreply.github.com>
2026-02-02 08:19:54 +0100

    Integrated API to HTML, including date, time, weather description and forecast emoji's

--- src/app.js
+++ src/app.js
@@ -1,3 +1,44 @@
+function adjustHeaderSize(cityName, header) {
+  let inputSplit = cityName;
+  console.log(inputSplit);
+  let inputLetterCount = inputSplit.length;
+  console.log(inputLetterCount);
+
+  if (inputLetterCount >= 10) {
+    header.classList.add(`smallerFont`);
+    if (inputLetterCount >= 16) {
+      header.classList.add(`smallestFont`);
+    }
+  } else {
+    header.classList.remove(`smallerFont`);
+    header.classList.remove(`smallestFont`);
+  }
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Thu", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function formatTime(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  console.log(date);
+  let time = `${hours}:${minutes}`;
+  return time;
+}
+
 function changeDisplay(response) {
   console.log(response.data);
 
@@ -5,57 +46,72 @@ function changeDisplay(response) {
   let temperature = document.querySelector("#degrees");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
+  let weatherIcon = document.querySelector("#weather-icon");
+  let mainDay = document.querySelector("#mainDay");
+  let mainTime = document.querySelector("#mainTime");
+  let weatherDescription = document.querySelector("#weather-description");
 
   h1.innerHTML = response.data.city;
   temperature.innerHTML = Math.round(response.data.temperature.current);
   humidity.innerHTML = `${response.data.temperature.humidity}%`;
   wind.innerHTML = `${Math.round(response.data.wind.speed * 10) / 10}km/h`;
+  console.log(response.data.condition.icon_url);
+  weatherIcon.innerHTML = `<img src="${response.data.condition.icon_url}" alt="main weather icon" class="mainEmoji">`;
+  mainDay.innerHTML = `${formatDay(response.data.time)}day`;
+  mainTime.innerHTML = `${formatTime(response.data.time)}`;
+  weatherDescription.innerHTML = response.data.condition.description;
 
-  let inputSplit = response.data.city.split(``);
-  console.log(inputSplit);
-  let inputLetterCount = inputSplit.length;
-  console.log(inputLetterCount);
-
-  if (inputLetterCount >= 10) {
-    h1.classList.add(`smallerFont`);
-  } else {
-    h1.classList.remove(`smallerFont`);
-  }
+  adjustHeaderSize(response.data.city, h1);
 }
 
-function userSubmit(event) {
-  event.preventDefault();
-  let cityInput = document.querySelector("#city-input");
-  console.log(cityInput.value);
-
-  let apiKey = `033479d334coa33b0517ta6857d46eaf`;
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${cityInput.value}&key=${apiKey}`;
-  axios.get(apiUrl).then(changeDisplay);
-}
-
-function displayForecast() {
+function displayForecast(response) {
   let days = ["Sun", "Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = ``;
+  let iconUrl = response.data.daily[0].condition.icon_url;
+  console.log(iconUrl);
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 6) {
+      forecastHtml =
+        forecastHtml +
+        `
       <div class="forecast-flexbox">
   <div class="day">
-      <div class="forecast-day figTree">${day}</div>
-   <div class="emoji">🌥️</div>
-    <div class="forecast-temperature">
-        <span class="head-temp">21°</span><span>12°</span>
+      <div class="forecast-day figTree">${formatDay(day.time)}</div>
+   <img src="${day.condition.icon_url}" alt="weather icons" class="emoji">
+    <div class="forecast-temperatures">
+        <span class="temp-max">${Math.round(day.temperature.maximum)}°</span><span class="temp-min">${Math.round(day.temperature.minimum)}°</span>
     </div>
     </div>
   </div>
 `;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
 }
 
+function getForecast(city) {
+  let apiForecastKey = `033479d334coa33b0517ta6857d46eaf`;
+  let apiForecastUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiForecastKey}`;
+  console.log(apiForecastUrl);
+  axios.get(apiForecastUrl).then(displayForecast);
+}
+
+function getWeather(city) {
+  let apiKey = `033479d334coa33b0517ta6857d46eaf`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(changeDisplay);
+}
+
+function userSubmit(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  getWeather(cityInput.value);
+  getForecast(cityInput.value);
+}
+
+getWeather("Paris");
+getForecast("Paris");
 let cityForm = document.querySelector("#city-form");
 cityForm.addEventListener("submit", userSubmit);
-displayForecast();
